cc61d208d60b29d12f14facca9765bb1084d46ce
William Saults <wsaults@email.itt-tech.edu>
2013-08-07 20:22:37 -0500

    Week 1: Updated launcher app to open app picker.

--- MDF3Launcher/src/com/fullsail/mdf3launcher/MainActivity.java
+++ MDF3Launcher/src/com/fullsail/mdf3launcher/MainActivity.java
@@ -2,7 +2,11 @@ package com.fullsail.mdf3launcher;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Switch views button
+		Button switchViewsButton = (Button) findViewById(R.id.launchApp);
+		switchViewsButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO: send selected city name;
+				Intent intent = new Intent("android.intent.action.MAIN");
+			    intent.setComponent(ComponentName.unflattenFromString("com.fullsail.weather"));
+			    intent.addCategory("android.intent.category.LAUNCHER");
+			    startActivity(intent);
+			}
+		});
 	}
 
 	@Override
